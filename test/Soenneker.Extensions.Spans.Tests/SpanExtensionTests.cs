ab3027190456e1dcf748160f78581eaae609a8d5
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:03 -0500

    TUnit conversion

--- test/Soenneker.Extensions.Spans.Tests/SpanExtensionTests.cs
+++ test/Soenneker.Extensions.Spans.Tests/SpanExtensionTests.cs
@@ -1,11 +1,10 @@
-﻿using Soenneker.Tests.Unit;
-using Xunit;
+using Soenneker.Tests.Unit;
 
 namespace Soenneker.Extensions.Spans.Tests;
 
 public sealed class SpanExtensionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
